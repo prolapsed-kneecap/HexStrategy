324b5d6e83b437c00f6f798ef7207a86e55e5cf9
79635 <2007eia2408@gmail.com>
2024-02-12 17:39:49 +0700

    hex

--- main.cpp
+++ main.cpp
@@ -1,25 +1,46 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+
+class Cell {
+public:
+    char symbol;
+protected:
+    void setSymbol(char);
+
+};
+
+void Cell::setSymbol(char c) {
+    this->symbol = c;
+}
+
+
+
+int main() {
     sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
+    sf::Texture texture;
+    texture.loadFromFile("C:\\Users\\79635\\CLionProjects\\HexStrategy\\images\\hex_2.png");
+    sf::Sprite hex;
+    sf::Sprite hex1;
+    sf::Sprite hex2;
+    hex.setTexture(texture);
+    hex1.setTexture(texture);
+    hex2.setTexture(texture);
+    hex.setPosition(0,0);
+    hex1.setPosition(94, 0);
+    hex2.setPosition(47, 28);
 
-    while (window.isOpen())
-    {
+
+    while (window.isOpen()) {
         sf::Event event;
-
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::White);
+        window.draw(hex);
+        window.draw(hex1);
+        window.draw(hex2);
         window.display();
     }
 }
